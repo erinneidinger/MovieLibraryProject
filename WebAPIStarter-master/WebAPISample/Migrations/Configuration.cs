83d13263e6ce9df1cc7379aac369239dc2f3a50a
Erinn Neidinger <erinndneidinger@gmail.com>
2020-01-13 13:25:24 -0600

    Seeded the database in configuration.cs

--- WebAPIStarter-master/WebAPISample/Migrations/Configuration.cs
+++ WebAPIStarter-master/WebAPISample/Migrations/Configuration.cs
@@ -18,6 +18,13 @@ namespace WebAPISample.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+        context.Movies.AddOrUpdate(
+        new Models.Movies { Title = "The Departed", Genre = "Drama", DirectorName = "Martin Scorsese" },
+        new Models.Movies { Title = "The Dark Knight", Genre = "Drama", DirectorName = "Christopher Nolan" },
+        new Models.Movies { Title = "Inception", Genre = "Drama", DirectorName = "Christopher Nolan" },
+        new Models.Movies { Title = "Pineapple Express", Genre = "Comedy", DirectorName = "David Gordon Green" },
+        new Models.Movies { Title = "Die Hard", Genre = "Action", DirectorName = "John McTiernan" }
+        );
         }
     }
 }
